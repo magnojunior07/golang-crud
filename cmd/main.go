15919da935af276c289922b8eba357fc6a383798
magnojunior07 <magnojunior319@gmail.com>
2023-03-01 12:45:31 -0400

    connect to database

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/magnojunior07/golang-crud/pkg/common/config"
+	"github.com/magnojunior07/golang-crud/pkg/common/db"
+	"github.com/spf13/viper"
+)
+	
 
 func main() {
-	fmt.Println("Hello, world!")
+	config.LoadConfig()
+	router := gin.Default()
+
+	port := viper.Get("PORT").(string)
+	dbUrl := viper.Get("DB_URL").(string)
+
+	db.Init(dbUrl)
+	router.Run(port)
+
+	fmt.Println("Hello, world")
 }
