4e6e4bad76b4e67016678061cc466c4259ef92c4
magnojunior07 <magnojunior319@gmail.com>
2023-02-28 20:40:10 -0400

    create connection to database

--- pkg/common/db/db.go
+++ pkg/common/db/db.go
@@ -3,5 +3,19 @@ package db
 import (
 	"log"
 
-	
+	"github.com/magnojunior07/golang-crud/pkg/common/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+func Init(url string) *gorm.DB {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&models.Team{})
+
+	return db
+}
