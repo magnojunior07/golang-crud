7aa7aae9ea8e696f65cfe3219f65d1c7d88050c2
Magno Júnior <magnojunior319@gmail.com>
2023-03-01 12:11:44 -0400

    Update song.go

--- pkg/common/models/song.go
+++ pkg/common/models/song.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type CharlieBrownJrSongs struct {
    gorm.Model
    SongName 	string `json:"songName"`
-   Album 		string `json:"qtd_titles"`
+   Album 	string `json:"qtd_titles"`
    Duration 	string `json:"duration"`
    LinkSpotify 	string `json:"linkSpotify"`
    LinkYouTube 	string `json:"linkYouTube"`
